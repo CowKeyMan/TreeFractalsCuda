0721fa3be1065bdbcb02afcb876ffbef4ece246d
cowkeyman <dancauchi1@gmail.com>
2020-05-02 23:19:04 +0200

    Updated parallel implementation - bug fix

--- Part 2 - Parallel Implementation/Implementation/src/FractalTreesParallel.cu
+++ Part 2 - Parallel Implementation/Implementation/src/FractalTreesParallel.cu
@@ -70,9 +70,9 @@ __global__ void calculate_points_single_iteration(
 {
   const unsigned long index = blockIdx.x * blockDim.x + threadIdx.x;
 
-  unsigned int array_index = index*2 + start_index; // first 2 already initialized
-  unsigned int array_index_plus_1 = array_index + 1; // first 2 already initialized
-  unsigned int array_index_div_2 = array_index >> 1;
+  unsigned long array_index = index*2 + start_index; // first 2 already initialized
+  unsigned long array_index_plus_1 = array_index + 1; // first 2 already initialized
+  unsigned long array_index_div_2 = array_index >> 1;
 
   angles[array_index] = ( (angles[array_index_div_2] + rotation_angle_degrees) ) % 360;
   angles[array_index_plus_1] = ( (angles[array_index_div_2] - rotation_angle_degrees) + 360 ) % 360;
@@ -120,7 +120,7 @@ __global__ void calculateMax(float *points, float *storeList, float *retValue, c
 
   for(int i = 1; i < iterations; ++i){ // start i from 1 as the first iteration has already been done
     if(index < no_of_threads){
-      storeList[index] = points[ index*2 + (points[index*2] < points[index*2 + 1]) ];
+      storeList[index] = storeList[ index*2 + (storeList[index*2] < storeList[index*2 + 1]) ];
       //storeList[index] = fmaxf(points[index*2], points[index*2 + 1]);
     }
     no_of_threads/=2;
@@ -137,7 +137,7 @@ __global__ void calculateMax_single_iteration(float *points, float *storeList){
 }
 
 __global__ void map_points_to_pixels(float *pointsX, float *pointsY, const float x_mul, const float x_add, const float y_mul, const float y_add){
-  const unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+  const unsigned long i = blockIdx.x * blockDim.x + threadIdx.x;
   pointsX[i] = pointsX[i] * x_mul + x_add;
   pointsY[i] = pointsY[i] * y_mul + y_add;
 }
@@ -150,7 +150,7 @@ __global__ void initialize_all_to_zero(short *m_image, int max_size){
 }
 
 __global__ void draw_points(const float *pointsX, const float *pointsY, short *m_image, const int image_height){
-  const unsigned long index = blockIdx.x * blockDim.x + threadIdx.x;
+  const unsigned long index = blockIdx.y * gridDim.x + blockIdx.x * blockDim.x + threadIdx.x;
   const unsigned long index_div_2 = index/2;
 
   // smaller x
@@ -198,8 +198,8 @@ int main(int argc, char *argv[]){
     failwith("Please enter a number of angle degrees between 0 and 180 (both included)");
   }
 
-  if(0 > iterations || iterations > 24){
-    failwith("Please enter a number of iterations between 1 and 24(both included)"); // otherwise a memory overflow occurs
+  if(0 > iterations || iterations > 26){
+    failwith("Please enter a number of iterations between 1 and 26(both included)"); // otherwise a memory overflow occurs
   }
 
   /* for testing
@@ -209,7 +209,7 @@ int main(int argc, char *argv[]){
   int iterations = 15; // (number of iterations) Must be between 1 and 26 (both included) otherwise it uses too much memory, as memory usage is 10x2^iterations bytes
   */
 
-  const unsigned long no_of_points = (1 << (iterations));
+  const unsigned long no_of_points = (1 << (iterations)); // 2^iterations lines + the original line
 
   // Declare the line length (after the first 11 iterations, since we use an internal line length in the gpu functions)
   float line_length = 1;
@@ -221,35 +221,42 @@ int main(int argc, char *argv[]){
   float *sin_map, *cos_map;
   const int map_size = (360 + 360%32); // 32 for the warp size
   const int map_physical_size = map_size * sizeof(float);
-  cudaMalloc((void**) &sin_map, map_physical_size);
-  cudaMalloc((void**) &cos_map, map_physical_size);
 
   // Declare angles and pointsX and pointsY lists (stored within the gpu)
   short *angles;
   float *pointsX, *pointsY;
   const unsigned long short_list_size = no_of_points * sizeof(short);
   const unsigned long float_list_size = no_of_points * sizeof(float);
-  cudaMalloc((void**) &angles, short_list_size);
-  cudaMalloc((void**) &pointsX, float_list_size);
-  cudaMalloc((void**) &pointsY, float_list_size);
 
   // initilize the first 2 of angles, pointsX and pointsY
   short angles_host[] = {0, 0};
   float pointsX_host[] = {0, 0};
   float pointsY_host[] = {0, 1};
-  cudaMemcpy(angles, angles_host, 2*sizeof(short), cudaMemcpyHostToDevice);
-  cudaMemcpy(pointsX, pointsX_host, 2*sizeof(float), cudaMemcpyHostToDevice);
-  cudaMemcpy(pointsY, pointsY_host, 2*sizeof(float), cudaMemcpyHostToDevice);
 
   float *minMax_X_Y; //Order: maxX, minY, maxY (we do not need minX as this is just maxX*-1)
   float *minMaxWorkingList;
-  cudaMalloc((void**) &minMax_X_Y, sizeof(float) * 3);
-  cudaMalloc((void**) &minMaxWorkingList, float_list_size/2);
 
   // --------- START TIMING PART 1 ----------
   double t = jbutil::gettime();
 
+  cudaMalloc((void**) &sin_map, map_physical_size);
+  cudaMalloc((void**) &cos_map, map_physical_size);
+
+  cudaMalloc((void**) &angles, short_list_size);
+  cudaMalloc((void**) &pointsX, float_list_size);
+  cudaMalloc((void**) &pointsY, float_list_size);
+
+  cudaMemcpy(angles, angles_host, 2*sizeof(short), cudaMemcpyHostToDevice);
+  cudaMemcpy(pointsX, pointsX_host, 2*sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(pointsY, pointsY_host, 2*sizeof(float), cudaMemcpyHostToDevice);
+
+  cudaMalloc((void**) &minMax_X_Y, sizeof(float) * 3);
+  cudaMalloc((void**) &minMaxWorkingList, float_list_size/2);
+
   populate_sin_cos_maps<<<1, map_size>>>(sin_map, cos_map);
+
+  // Calculate the new points which the lines will connect to
+  // Calculate the first 2048
   calculate_points<<<1, 512>>>(
     angles,
     pointsX,
@@ -260,6 +267,8 @@ int main(int argc, char *argv[]){
     sin_map,
     cos_map
   );
+
+  // calculate the rest, iteration by iteration
   for(int i = 2048; i < no_of_points; i *= 2){
     calculate_points_single_iteration<<<i/1024, 512>>>(
       angles,
@@ -274,13 +283,14 @@ int main(int argc, char *argv[]){
     );
   }
 
+  // Find Max X
   bool firstTime = true;
-  for(int i = no_of_points/2; i > 512; i /= 2){
+  for(int i = no_of_points/2; i > 1024; i /= 2){
     if(firstTime){
-      calculateMax_single_iteration<<<i/1024, 512>>>(pointsX, minMaxWorkingList);
+      calculateMax_single_iteration<<<i/512, 512>>>(pointsX, minMaxWorkingList);
       firstTime = false;
     }else{
-      calculateMax_single_iteration<<<i/1024, 512>>>(minMaxWorkingList, minMaxWorkingList);
+      calculateMax_single_iteration<<<i/512, 512>>>(minMaxWorkingList, minMaxWorkingList);
     }
   }
   if(firstTime){
@@ -288,9 +298,10 @@ int main(int argc, char *argv[]){
   }else{
     calculateMax<<<1, 512>>>(minMaxWorkingList, minMaxWorkingList, &minMax_X_Y[0], iterations, no_of_points/2);
   }
-
+  cudaError_t error = cudaGetLastError();if(error != cudaSuccess){printf("CUDA error1: %s\n", cudaGetErrorString(error));exit(-1);}
+  // Find Min Y
   firstTime = true;
-  for(int i = no_of_points/2; i > 512; i /= 2){
+  for(int i = no_of_points/2; i > 1024; i /= 2){
     if(firstTime){
       calculateMin_single_iteration<<<i/512, 512>>>(pointsY, minMaxWorkingList);
       firstTime = false;
@@ -304,8 +315,9 @@ int main(int argc, char *argv[]){
     calculateMin<<<1, 512>>>(minMaxWorkingList, minMaxWorkingList, &minMax_X_Y[1], iterations, no_of_points/2);
   }
 
+  // Find Max Y
   firstTime = true;
-  for(int i = no_of_points/2; i > 512; i /= 2){
+  for(int i = no_of_points/2; i > 1024; i /= 2){
     if(firstTime){
       calculateMax_single_iteration<<<i/512, 512>>>(pointsY, minMaxWorkingList);
       firstTime = false;
@@ -318,9 +330,9 @@ int main(int argc, char *argv[]){
   }else{
     calculateMax<<<1, 512>>>(minMaxWorkingList, minMaxWorkingList, &minMax_X_Y[2], iterations, no_of_points/2);
   }
-
   float maxX_minY_maxY[3];
   cudaMemcpy(maxX_minY_maxY, minMax_X_Y, sizeof(float) * 3, cudaMemcpyDeviceToHost);
+  cout << maxX_minY_maxY[0] << " " << maxX_minY_maxY[1] << " " << maxX_minY_maxY[2] << endl;
   float x_mul = (maxX_minY_maxY[0] == 0)? 1: (image_width-1)/(maxX_minY_maxY[0]*2);
   float x_add = (image_width-1)/2.0f;
   float y_mul = (maxX_minY_maxY[2]==maxX_minY_maxY[1])? (image_height-1) : -(image_height-1)/(maxX_minY_maxY[2] - maxX_minY_maxY[1]);
@@ -335,9 +347,6 @@ int main(int argc, char *argv[]){
   cudaMemcpy(a, angles, short_list_size, cudaMemcpyDeviceToHost);
   cudaMemcpy(px, pointsX, float_list_size, cudaMemcpyDeviceToHost);
   cudaMemcpy(py, pointsY, float_list_size, cudaMemcpyDeviceToHost);
-  // --------- STOP TIMING PART 1 ----------
-  t = jbutil::gettime() - t;
-  std::cerr << "Time taken to generate the points: " << t << "s" << endl;
 
   // Clean up
   cudaFree(sin_map);
@@ -346,20 +355,21 @@ int main(int argc, char *argv[]){
   cudaFree(minMax_X_Y);
   cudaFree(minMaxWorkingList);
 
+  // --------- STOP TIMING PART 1 ----------
+  t = jbutil::gettime() - t;
+  std::cerr << "Time taken to generate the points: " << t << "s" << endl;
+
+
   short *m_image;
-  cudaMalloc((void**) &m_image, sizeof(short) * image_height * image_width);
 
   // --------- START TIMING PART 2 ----------
   t = jbutil::gettime();
 
+  cudaMalloc((void**) &m_image, sizeof(short) * image_height * image_width);
+
   initialize_all_to_zero<<<(image_width*image_height)/512 + ((image_width*image_height)%512 != 0), 512>>>(m_image, image_width*image_height);
   draw_points<<<(no_of_points<512)? 1 : no_of_points/512, ((no_of_points<512)? no_of_points : 512)>>>(pointsX, pointsY, m_image, image_height);
 
-  // --------- STOP TIMING PART 2 ----------
-  t = jbutil::gettime() - t;
-  std::cerr << "Time taken to generate the points: " << t << "s" << endl;
-
-  //SAVE
   // copy from device to host
   short *m_image_host = (short*)malloc(sizeof(short)*image_width*image_height);
   cudaMemcpy(m_image_host, m_image, sizeof(short)*image_width*image_height, cudaMemcpyDeviceToHost);
@@ -369,7 +379,7 @@ int main(int argc, char *argv[]){
 
   // copy from flattened array to image matrix
   for(int x = 0; x < image_width*image_height; ++x){
-	  m_image_2(x%image_height, x/image_height) = m_image_host[x];
+      m_image_2(x%image_height, x/image_height) = m_image_host[x];
   }
 
   //Clean up
@@ -380,6 +390,12 @@ int main(int argc, char *argv[]){
 
   image<int> image_out = image<int>(image_height, image_width, 1, 255);
   image_out.set_channel(0, m_image_2);
+
+  // --------- STOP TIMING PART 2 ----------
+  t = jbutil::gettime() - t;
+  std::cerr << "Time taken to generate the points: " << t << "s" << endl;
+
+  //SAVE
   char outfile[150];
   sprintf(outfile, "output_images/%d x%d _m=%.2f _theta=%d _n= %.0d.pgm",
     image_width, image_height,
@@ -392,4 +408,3 @@ int main(int argc, char *argv[]){
 }
 //cudaDeviceSynchronize(); size_t fr, tot; cudaMemGetInfo(&fr, &tot); cout << "mem " << fr << " " << tot << endl;
 //cudaError_t error = cudaGetLastError();if(error != cudaSuccess){printf("CUDA error1: %s\n", cudaGetErrorString(error));exit(-1);}
-
